8298db04002c1ac4e30a4068e97f0ee4f7303d74
Ryan S <ryansateles@gmail.com>
2022-08-05 15:57:19 -0300

    feature: set hashCode Method to comparasion

--- src/main/java/com/opussoftware/simpleapp/domain/User.java
+++ src/main/java/com/opussoftware/simpleapp/domain/User.java
@@ -64,4 +64,17 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
