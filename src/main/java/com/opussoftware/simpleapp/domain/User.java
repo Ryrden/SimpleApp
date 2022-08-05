41639f802c600aa3de2ee38befdda99985747b8f
Ryan S <ryansateles@gmail.com>
2022-08-05 14:22:57 -0300

    User domain class created

---
+++ src/main/java/com/opussoftware/simpleapp/domain/User.java
@@ -0,0 +1,56 @@
+package com.opussoftware.simpleapp.domain;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "name cannot be empty")
+    @Length(min = 2,max = 100, message = "name must be between 2 and 100")
+    private String name;
+    @NotEmpty(message = "login cannot be empty")
+    private String login;
+    @NotEmpty(message = "password cannot be empty")
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
