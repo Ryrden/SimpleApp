bf498541c73cb05f004e345c0fcba2a034ba6739
Ryan S <ryansateles@gmail.com>
2022-08-05 15:55:17 -0300

    feat: JPA example with CommandLineRunner run Method

--- src/main/java/com/opussoftware/simpleapp/SimpleAppApplication.java
+++ src/main/java/com/opussoftware/simpleapp/SimpleAppApplication.java
@@ -1,12 +1,28 @@
 package com.opussoftware.simpleapp;
 
+import com.opussoftware.simpleapp.domain.User;
+import com.opussoftware.simpleapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SimpleAppApplication {
+public class SimpleAppApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
     public static void main(String[] args) {
         SpringApplication.run(SimpleAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User userOne = new User(null, "Ryan Souza", "Ryan", "123");
+        User userTwo = new User(null, "Lourenço Henrique", "Lourenço", "123");
+
+        userRepository.saveAll(Arrays.asList(userOne,userTwo));
+    }
 }
