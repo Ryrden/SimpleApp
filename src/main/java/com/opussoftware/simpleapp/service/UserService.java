927f6983e6c40b142a5866ef3289d394f029fcc7
Ryan S <ryansateles@gmail.com>
2022-08-08 10:23:24 -0300

    feat: Handling Object not found Exceptions

--- src/main/java/com/opussoftware/simpleapp/service/UserService.java
+++ src/main/java/com/opussoftware/simpleapp/service/UserService.java
@@ -2,6 +2,7 @@ package com.opussoftware.simpleapp.service;
 
 import com.opussoftware.simpleapp.domain.User;
 import com.opussoftware.simpleapp.repositories.UserRepository;
+import com.opussoftware.simpleapp.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,6 @@ public class UserService {
 
     public User findById(Integer id){
         Optional<User> object = repository.findById(id);
-        return object.orElse(null);
+        return object.orElseThrow( () -> new ObjectNotFoundException("Object not found! Id: " + id + ", Type: " + User.class.getName()));
     }
 }
